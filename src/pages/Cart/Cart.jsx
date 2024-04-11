8bb11f8d4564444699c9f41654d42b5e353e8105
Deepak Singh Chauhan <85275831+Deepak3440@users.noreply.github.com>
2024-04-12 02:02:42 +0530

    Update Cart.jsx

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -29,9 +29,9 @@ const Cart = () => {
               <div className='cart-items-item1' key={item._id}>
                 <img src={item.image} alt="" />
                 <p>{item.name}</p>
-                <p>{item.price}</p>
+                <p>Rs{item.price}</p>
                 <p>{quantity}</p>
-                <p>{item.price * quantity}</p>
+                <p>Rs{item.price * quantity}</p>
                 <button onClick={() => removeFromCart(item._id)}>x</button>
               </div>
             );
