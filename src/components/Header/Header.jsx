d3551749575ceb5f3053dd3a7ff933b95d3b64d4
Deepak Singh Chauhan <85275831+Deepak3440@users.noreply.github.com>
2024-04-12 01:59:36 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='header'>
         <div className="header-contents">
-            <h2>Order Your Fav Food</h2>
+            <h2>Khana mangoge khana Denge</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus fugiat minus rerum dolorum totam soluta sint itaque ex rem reiciendis?</p>
             <button>View Menu</button>
         </div>
